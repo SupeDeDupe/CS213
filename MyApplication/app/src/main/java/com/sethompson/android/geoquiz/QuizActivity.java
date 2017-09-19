22f0967a69508d1b00865d3db99a8abb65fac69d
SupeDeDupe <sethompson@upei.ca>
2017-09-19 18:27:04 -0300

    Added buttons, linear layouts, and textviews

--- MyApplication/app/src/main/java/com/sethompson/android/geoquiz/QuizActivity.java
+++ MyApplication/app/src/main/java/com/sethompson/android/geoquiz/QuizActivity.java
@@ -3,11 +3,17 @@ package com.sethompson.android.geoquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Created by sethompson on 2017-09-19.
+ */
+
 public class QuizActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
     }
 }
+
